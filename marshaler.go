017fdf639300e4ce382c453012939a4082473843
Bruno Gomes <gomes.bruno.ac@gmail.com>
2026-05-14 14:53:48 -0300

    fix: Set deterministic marshaling in proto marshaler

--- marshaler.go
+++ marshaler.go
@@ -6,36 +6,6 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type ProtoConstraint[T any] interface {
-	proto.Message
-	*T
-}
-
-/*
-// Marshaler implementation for protobuff objects
-type ProtoMarshaler[T any, PT ProtoConstraint[T]] struct {}
-
-
-func (m *ProtoMarshaler[T, PT]) Marshal(obj PT) ([]byte, error) {
-    bytes, err := proto.Marshal(obj)
-    if err != nil {
-        return nil, err
-    }
-    return bytes, nil
-}
-
-func (m *ProtoMarshaler[T, PT]) Unmarshal(bytes []byte) (T, error) {
-    var obj T
-    p := PT(&obj)
-
-    err := proto.Unmarshal(bytes, p)
-    if err != nil {
-        return obj, err
-    }
-    return obj, nil
-}
-*/
-
 // Marshaler implementation for protobuff objects
 type factoryProtoMarshaler[T proto.Message] struct {
 	factory func() T
@@ -48,7 +18,7 @@ func ProtoMarshaler[T proto.Message](factory func() T) Marshaler[T] {
 }
 
 func (m *factoryProtoMarshaler[T]) Marshal(obj *T) ([]byte, error) {
-	bytes, err := proto.Marshal(*obj)
+	bytes, err := proto.MarshalOptions{Deterministic: true}.Marshal(*obj)
 	if err != nil {
 		return nil, err
 	}
